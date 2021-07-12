f150f008d18acfec12a9ff82d03f2b8e7c09ebf4
Bryan Duncanson <bduncanson@madisoncollege.edu>
2021-07-11 18:37:47 -0700

    Finshed boolean.py and did some playing around

---
+++ week3/boolean.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+print(True and True)
+print(False and True)
+print(bool(1==1) and bool(2==1))
+print(bool(0))
+print(bool(""))
+print(bool(0.0))
+print(not 0)
+print("test"=="test")
+print(1==1 or 2!=1)
+print(True and 1==1)
+print(False and 0!=0)
+print(True or 1==1)
+print("test" == "testing")
+print(1 != 0 and 2 == 1)
+print("test" != "testing")
+print("test" == 1)
+print(1 == 1 and not ("testing" == 1 or 1 == 0))
+print("chunky" == "bacon" and not (3 == 4 or 3 == 3))
+
+
+
+
