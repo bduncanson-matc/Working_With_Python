cf314d5e441041a6cae86d0cead164baf401cf46
Bryan Duncanson <bduncanson@madisoncollege.edu>
2021-07-15 08:53:10 -0500

    Starting midterm-if

---
+++ week4/midterm-if.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#Variables
+firstName = "Bryan"
+lastName = "Duncanson"
+openMidtermTxt = open("Midterm-if.txt")
+midTermLines = (openMidtermTxt.readlines())
+midTermLine = (openMidtermTxt.readline())
+#Total add total when I figured out what to define it as
+
+#list
+keyWordsList = {"gmeach18@ed.gov",
+"248.253.63.149",
+"Whiteland",
+"80.222.19.190",
+"Kayley",
+"dcassyqw@microsoft.com"}
+
+
+#script code
+print (f"Name: {firstName:<>6}>{lastName:<>10}>")
+
+
+
+
+
+
