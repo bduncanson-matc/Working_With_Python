00dc13ab07ba2a674f9f1d74b239f03dd8ca9989
Bryan Duncanson <bduncanson@madisoncollege.edu>
2021-07-15 20:21:55 -0500

    Program completed, if time premits more testing and refactoring could be done.

--- week4/midterm-jpark.py
+++ week4/midterm-jpark.py
@@ -1,23 +1,46 @@
 #! /usr/bin/env python3
-#Variables
+# Variables
 firstName = "Bryan"
 lastName = "Duncanson"
 password_database = {
-    "Username" : "dnedry",
-    "Password" : "please"
+    "Username": "dnedry",
+    "Password": "please"
 }
 command_database = {
-    "reboot" : "OK. I will reboot all park system.",
-    "shutdown" : "Ok. I will shot down all park systems.",
-    "done" : "I hate all this hacker stuff.",
+    "a. reboot": "OK. I will reboot all park system.",
+    "b. shutdown": "Ok. I will shot down all park systems.",
+    "c. done": "I hate all this hacker stuff.",
 }
+listPWDBKeys = list(password_database)
+listCDBKeys = list(command_database)
+magicWord = "You Didnt say the magic word"
 white_rabbit_object = 0
 counter = 0
-#Populate the dictionaries
 
+# Driver code
+print(f"Name: {firstName:<>6}>{lastName:<>10}>")
 
-
-
-
-#code
-print(command_database["Username"])
+while counter < 3 and white_rabbit_object == 0: # counter less 3 and wro == 0
+    input_user = input(f"{listPWDBKeys[0]::<9}") # getting practice with fstrings
+    input_password = input(f"{listPWDBKeys[1]::<9}")
+    if input_user == password_database["Username"] and input_password == password_database["Password"]:# username/pw validation
+            white_rabbit_object = 1 # allowing user to proceed
+            print("Hi, Dennis.  You're still the best hacker in Jurassic Park.")# This move was made in the early 90s and at that time they taught me 2 spaces after a period then :D
+    else:
+        counter += 1 # increment to limit limited attempts
+        print(f"{magicWord}. {counter}")# add failure string plus pw count
+if white_rabbit_object == 1:  # Succesful athetication loop
+    for key in command_database:
+        print(f"{key}")
+    user_command = input("Please enter a command: ")
+    if user_command == listCDBKeys[0] or user_command.lower() == "a" or user_command.lower == "reboot":
+        print(command_database[listCDBKeys[0]])
+    elif user_command == listCDBKeys[1] or user_command.lower() == "b" or user_command.lower == "shutdown":
+        print(command_database[listCDBKeys[1]])
+    elif user_command == listCDBKeys[2] or user_command.lower() == "c" or user_command.lower == "done":
+        print(command_database[listCDBKeys[2]])
+    else:
+        print("The Lysine Contingeny has been put in to effect.")
+elif counter == 3:
+    newLineMagicWord = f"{magicWord}!\n"
+    print(f"{newLineMagicWord*25}")
