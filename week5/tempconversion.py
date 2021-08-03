be9c9dcc1e6313dfd24986ab8fbdee1d4ee958b4
bduncanson-matc <86499328+bduncanson-matc@users.noreply.github.com>
2021-08-03 14:16:29 -0500

    Update tempconversion.py

--- week5/tempconversion.py
+++ week5/tempconversion.py
@@ -8,7 +8,7 @@ loop = 0
 temp = float(input("Enter a temperature: "))# should add digits but need to finish atm life is hard.
 while loop == 0:
     scale = input("Enter the measurement scale of (F or C): ")# could add more strings that signify the degree
-    counter = 1
+    loop = 1
     break
 
 #after a degree is chosen
@@ -18,7 +18,7 @@ while loop == 1:
         print(f"{temp} in Fahrenheit is equal to {tempC} in Celsius")
         reset = input("Would you like to convert another temperature?(Y/N): ")
         if reset.lower() == "y":
-            counter = 0
+            loop = 0
         else:
             break
     else:
@@ -26,7 +26,7 @@ while loop == 1:
         print(f"{temp} in Celsius is equal to {tempF} in Fahrenheit")
         reset = input("Would you like to convert another temperature?(Y/N): ")
         if reset.lower() == "y":
-            counter = 0
+            loop = 0
         else:
             break  # need to fix the loop logic above but dont have the time :(
 
