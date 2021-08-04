379be3bc635c66de3d997178324095311b412094
Bryan Duncanson <bduncanson@madisoncollege.edu>
2021-08-03 18:09:48 -0700

    Subprocess assignment completed

---
+++ week5/subprocesses.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import subprocess
+
+myProc = subprocess.run(['ps', '-axco', 'command'], stdout=subprocess.PIPE)
+myProcList = myProc.stdout.decode().split('\n')
+
+
+for item in myProcList:
+    print(f"{item}")
+    count += 1
+
+count = 0
+for item in myProcList[1:]:
+    count += 1
+
+print(count)
+
+# seemed too easy....
