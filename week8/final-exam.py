4448d9da45eb81dac3217b9e3089427221cdf00a
Bryan Duncanson <bduncanson@madisoncollege.edu>
2021-08-09 20:14:38 -0500

    added function 3

--- week8/final-exam.py
+++ week8/final-exam.py
@@ -4,20 +4,22 @@
 import argparse
 import requests
 from bs4 import BeautifulSoup
+import json
 
 # basic variables
 firstName = "Bryan"
 lastName = "Duncanson"
+fullName = f"{firstName} {lastName}"
 # adding arguments using the argparse module
 parser = argparse.ArgumentParser(description='final exam arg parse')
 parser.add_argument('-ip', required= True, type=str, dest='ipVar', default='172.23.23.252', help="Enter a ip address")
-parser.add_argument('-f', required= True, choices=[1,2,3,4,5], type=int, dest='funcVar', help="Enter a function number")
+parser.add_argument('-f', required= True, choices=[1, 2, 3, 4, 5], type=int, dest='funcVar', help="Enter a function number")
 # creating a variable to access arguments
 args = parser.parse_args()
 # url string variable using arguments from the terminal
 url = f"http://{args.ipVar}/q{args.funcVar}"
 
-print(f"{firstName} {lastName}")
+print(fullName)
 print(url)
 
 # function to return text of the url in the -ip argument
@@ -25,8 +27,8 @@ def get_response():
     response = requests.get(url)
     return response.text
 # call if the argugment -f == 1
-if args.funcVar == 1:
-    print(get_response())
+# if args.funcVar == 1:
+#   print(get_response())
 
 def parse_string():
     response = requests.get(url)
@@ -37,5 +39,23 @@ def parse_string():
 
     return hStr[2::3]
 
-if args.funcVar == 2:
-    print(parse_string())
+#if args.funcVar == 2:
+ #   print(parse_string())
+
+
+
+def parse_header(arg=url):
+    response = requests.get(arg)
+    headerDict = response.headers
+    return headerDict['MATC-HEADER']
+
+def parse_json(arg=url):
+    response = requests.get(arg)
+    jsonDict = json.loads(response.text)
+    decodedDict = complex(jsonDict)
+    return response
+
+funcKey = args.funcVar - 1
+funcList = [get_response, parse_string, parse_header]
+print(funcList[funcKey]())
+print(parse_json())
